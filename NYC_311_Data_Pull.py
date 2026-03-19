6bd7006815f910de52e307d8a64c98885bf0a1df
Diego Sanchez <diegos@bu.edu>
2026-03-18 22:36:13 -0400

    Code updated to call Socrata API directly, due to deprecation of sodapy

--- NYC_311_Data_Pull.py
+++ NYC_311_Data_Pull.py
@@ -5,35 +5,30 @@ from dotenv import load_dotenv
 
 import pandas as pd
 from datetime import datetime, timezone, timedelta
-from sodapy import Socrata
+import requests
 
 load_dotenv()
 
 app_token = os.getenv('NYC_APP_TOKEN')
 
 def main():
-    # Set Socrata client parameters
-    client = Socrata(
-        'data.cityofnewyork.us',
-        app_token,
-        timeout=60
-    )
-
     # Set current UTC date minus 2 days
     date_requested = (datetime.now(timezone.utc) + timedelta(days=-2)).replace(hour=0, minute=0, second=0, microsecond=0).strftime('%Y-%m-%dT%H:%M:%S')
-
-    # Get 311 service requests from last 2 days
-    with client:
-        results = client.get(
-            'erm2-nwe9', 
-            where = f"created_date >= '{date_requested}'",
-            limit = 5,
-            order = 'created_date DESC'
-        )
     
-    # Convert results to dataframe
-    df = pd.DataFrame(results)
+    url = 'https://data.cityofnewyork.us/api/v3/views/erm2-nwe9/query.json'
 
+    data = requests.post( 
+                        url,
+                        headers = { 'X-App-Token': f'{app_token}' },
+                        json = { 'query': f"SELECT unique_key, status, created_date, agency, complaint_type, descriptor, incident_address, incident_zip, \
+                                                    city, resolution_action_updated_date, resolution_description, closed_date  \
+                                            WHERE created_date >= '{date_requested}' \
+                                                OR resolution_action_updated_date >= '{date_requested}' \
+                                            ORDER BY created_date DESC"
+                                }
+                    ).json()
     
+    df = pd.DataFrame(data).filter(regex='^[^:].*')
+        
 main()
 
